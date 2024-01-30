657604da26a84c46268e703761cd8c54ff322986
Mashkhurbek <mashkhurbek.mashrabov@gmail.com>
2024-01-30 12:03:13 +0500

    added articles

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
+import os
+
 from flask import Flask, render_template
+from slugify import slugify
 
 app = Flask(__name__)
 
+
+def read_articles():
+    articles = os.listdir('articles')
+    article_dict = {}
+    for article in articles:
+        article_dict.update({slugify(article): article})
+    return article_dict
+
+
 @app.route('/')
 def blog():
-    return render_template('blog.html')
+    return render_template('blog.html', articles=read_articles())
 
 @app.route('/blog/<slug>')
 def article(slug: str):
-    return render_template('article.html')
-
+    articles = read_articles()
+    article = articles[slug]
+    with open('articles/' + article, 'r') as article_content:
+        content = article_content.read()
+    return render_template('article.html', content=content, title=article)
 
 
 if __name__ == '__main__':
